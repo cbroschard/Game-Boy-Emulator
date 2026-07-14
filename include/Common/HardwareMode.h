3c2d7917bacb1b9c234f16545402510fb5ed3956
Christopher Broschard <cbroschard@hotmail.com>
2026-07-14 13:48:29 -0400

    HardwareMode: Initial commit

---
+++ include/Common/HardwareMode.h
@@ -0,0 +1,41 @@
+#ifndef HARDWAREMODE_H_INCLUDED
+#define HARDWAREMODE_H_INCLUDED
+
+#include <cstdint>
+
+enumb class HardwareMode : uint8_t
+{
+    DMG,
+    CGB
+}
+
+enum class CartridgeColorSupport : uint8_t
+{
+    DMGOnly,
+    CGBCompatible,
+    CGBOnly
+};
+
+constexpr bool isCGBMode(HardwareMode mode)
+{
+    return mode == HardwareMode::CGB;
+}
+
+constexpr bool isDMGMode(HardwareMode mode)
+{
+    return mode == HardwareMode::DMG;
+}
+
+constexpr bool supportsCGB(CartridgeColorSupport support)
+{
+    return support == CartridgeColorSupport::CGBCompatible ||
+           support == CartridgeColorSupport::CGBOnly;
+}
+
+constexpr bool supportsDMG(CartridgeColorSupport support)
+{
+    return support == CartridgeColorSupport::DMGOnly ||
+           support == CartridgeColorSupport::CGBCompatible;
+}
+
+#endif // HARDWAREMODE_H_INCLUDED
