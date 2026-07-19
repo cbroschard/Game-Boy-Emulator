32d169ee947825a974329d56a57b90aff0f5f95c
Christopher Broschard <cbroschard@hotmail.com>
2026-07-19 01:17:20 -0400

    Version 0.0.0.2-alpha

--- include/Version.h
+++ include/Version.h
@@ -11,7 +11,7 @@
 struct VersionInfo
 {
     static constexpr const char* NAME = "GameBoy Emulator by Christopher Broschard";
-    static constexpr const char* VERSION = "v0.0.0.1-alpha";
+    static constexpr const char* VERSION = "v0.0.0.2-alpha";
     static constexpr const char* BUILD_DATE = __DATE__;
     static constexpr const char* BUILD_TIME = __TIME__;
 };
